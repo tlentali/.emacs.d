36ba727b684e3300ae1005dd4ed7a31352d8248e
tlentali <thomas.lentali@gmail.com>
2020-08-31 23:15:53 +0200

    python cleaning conf

--- programming_conf/python_conf.el
+++ programming_conf/python_conf.el
@@ -1,13 +1,17 @@
 ;;; elpy
 (elpy-enable)
+
 ;;; use flycheck not flymake with elpy
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 ;;; get python3 interpreter
 ;; (setq python-shell-interpreter "/usr/bin/python3")
+
 ;;; emacs ipython notebook
 ;;(require 'ein)
+
 ;;; yasnippet
 (use-package yasnippet
   :ensure t
