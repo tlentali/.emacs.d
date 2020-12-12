9fc72f136d72f995c92a8b4083c119b0805fe46a
tlentali <thomas.lentali@gmail.com>
2020-12-12 23:34:21 +0100

    remove indicator of flycheck in margin

--- programming_conf/python_conf.el
+++ programming_conf/python_conf.el
@@ -6,6 +6,9 @@
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
+;;; remove flycheck margin indicator (https://www.flycheck.org/en/latest/user/error-reports.html)
+(setq-default flycheck-indication-mode 'nil)
+
 ;;; get python3 interpreter
 ;; (setq python-shell-interpreter "/usr/bin/python3")
 
