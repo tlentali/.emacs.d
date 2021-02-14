58be1089a78bbe991ce913265e7858b25b1f018c
tlentali <thomas.lentali@gmail.com>
2021-02-14 17:42:19 +0100

    add file orga for each language

--- init.el
+++ init.el
@@ -7,6 +7,14 @@
 
 (setq main_conf (file-name-as-directory "main_conf"))
 (setq programming_conf (file-name-as-directory "programming_conf"))
+
+(setq python (file-name-as-directory "python"))
+(setq markdown (file-name-as-directory "markdown"))
+(setq csv (file-name-as-directory "csv"))
+(setq org (file-name-as-directory "org"))
+(setq magit (file-name-as-directory "magit"))
+(setq dired (file-name-as-directory "dired"))
+
 (setq theme (file-name-as-directory "theme"))
 (setq snippet (file-name-as-directory "snippets"))
 
@@ -27,24 +35,41 @@
 
 ;; python
 ;;-------
-(setq path_python_conf (concat root programming_conf "python_conf.el"))
+(setq path_python_conf (concat root programming_conf python "python_conf.el"))
 (load path_python_conf)
 
 ;; org
 ;;-------
-(setq path_org_conf (concat root programming_conf "org_conf.el"))
+(setq path_org_conf (concat root programming_conf org "org_conf.el"))
 (load path_org_conf)
 
 ;; magit
 ;;-------
-(setq path_magit_conf (concat root programming_conf "magit_conf.el"))
+(setq path_magit_conf (concat root programming_conf magit "magit_conf.el"))
 (load path_magit_conf)
 
 ;; csv
 ;;----
-(setq path_csv_conf (concat root programming_conf "csv_conf.el"))
+(setq path_csv_conf (concat root programming_conf csv "csv_conf.el"))
 (load path_csv_conf)
 
 ;; theme
 ;;------
 (add-to-list 'custom-theme-load-path (concat root theme))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (doom-modeline zoom zerodark-theme yasnippet-snippets yascroll writeroom-mode window-numbering use-package-ensure-system-package undo-tree treemacs swiper speed-type spaceline smooth-scrolling smex smartparens shrink-path rainbow-delimiters py-autopep8 projectile popup-kill-ring org-plus-contrib org-journal neotree multiple-cursors mood-line markdown-toc magit jupyter impatient-mode ido-vertical-mode horizon-theme golden-ratio gist flycheck expand-region elpy ein dimmer diff-hl dashboard ctrlf color-theme-sanityinc-tomorrow beacon)))
+ '(zoom-mode t nil (zoom))
+ '(zoom-size (quote (0.618 . 0.618))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t (:foreground "#f60386" :background "#2d2d2d")))))
--- programming_conf/org/org_conf.el
+++ programming_conf/org/org_conf.el
@@ -1,10 +1,16 @@
 (require 'org)
-;;; org agenda
-;;(setq org-agenda-files (quote ("~/Dropbox/alfred/agenda/")))
+
+;;; set path
+(setq home (file-name-as-directory "~"))
+(setq dropbox (file-name-as-directory "Dropbox"))
+(setq alfred (file-name-as-directory "alfred"))
+(setq root_org (concat home dropbox alfred))
+
+(setq org-directory root_org)
 
 (load-library "find-lisp")
 (setq org-agenda-files
-   (find-lisp-find-files "~/Dropbox/alfred/" "\.org$"))
+   (find-lisp-find-files root_org "\.org$"))
 
 ;;(setq org-agenda-files (directory-files-recursively "~/.personal/agenda/" "~/.personal/journal" "\\.org$"))
 (bind-key "C-c a" #'org-agenda)
@@ -25,7 +31,8 @@
 (use-package org-contacts
   :ensure nil
   :after org
-  :custom (org-contacts-files '("~/Dropbox/alfred/contact/contacts.org")))
+  :custom (org-contacts-files '(concat root_org "contact/contacts.org")))
+
 (use-package org-capture
   :ensure nil
   :after org
@@ -40,11 +47,12 @@
 :END:" "Template for org-contacts.")
   :custom
   (org-capture-templates
-   `(("c" "Contact" entry (file+headline "~/Dropbox/alfred/contact/contacts.org" "Contacts"),
+   `(("c" "Contact" entry (file+headline (concat root_org "contact/contacts.org") "Contacts"),
       my/org-contacts-template
       :empty-lines 1))))
 
 ;;; good looking agenda M-x org-agenda RET o RET
+;;; or C-c a o
 ;; https://www.reddit.com/r/unixporn/comments/jprtoj/xmonad_productivity_workflow_emacs_orgmode_agenda/
 (setq org-agenda-custom-commands
       '(("o" "My Agenda"
@@ -71,13 +79,32 @@
           ))))
 ;;; org journal
 (require 'org-journal)
-(setq org-journal-dir "~/Dropbox/alfred/journal/")
+(setq org-journal-dir (concat "/journal/"))
 (setq org-journal-file-format "%Y%m%d.org")
 (setq org-journal-date-format "%A, %d %B %Y")
 (setq org-journal-enable-agenda-integration t)
 
-;;; mobileorg settings
-(setq org-directory "~/Dropbox/personal/")
-(setq org-mobile-inbox-for-pull "~/Dropbox/personal/agenda/todo.org")
-(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
-(setq org-mobile-files '("~/Dropbox/personal/"))
+(setq org-default-notes-file (concat root_org "refile/refile.org"))
+
+;; I use C-c c to start capture mode
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+;; https://emacs.stackexchange.com/a/42140
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("n" "note" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree (lambda () (concat root_org "refile/diary.org")))
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file (lambda () (concat root_org "refile/refile.org")))
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (fiqle (lambda () (concat root_org "refile/refile.org")))
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
